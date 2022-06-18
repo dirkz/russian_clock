e0480f3c2ca25a08140d026d7ed629f200fe0648
Dirk Zimmermann <me@dirkz.com>
2022-06-18 09:34:45 +0200

    Helper function

--- lib/clock.dart
+++ lib/clock.dart
@@ -19,27 +19,41 @@ class Clock extends CustomPainter {
     final center = Offset(size.width / 2, size.height / 2);
     canvas.drawCircle(center, r, pCircle);
 
-    final pHourMark = Paint();
-    pHourMark.style = PaintingStyle.fill;
-    pHourMark.strokeWidth = 4;
+    final paintHourMark = Paint();
+    paintHourMark.style = PaintingStyle.fill;
+    paintHourMark.strokeWidth = 4;
 
-    const hourLineLength = 20;
-    const hourRadSpacing = 2 * pi / 12;
-    for (double rad = 0; rad < 2 * pi; rad = rad + hourRadSpacing) {
-      final offsetOuter = _offsetOnCircle(
-          radius: r, radians: rad, xOffset: center.dx, yOffset: center.dy);
-      final offsetInner = _offsetOnCircle(
-          radius: r - hourLineLength,
-          radians: rad,
-          xOffset: center.dx,
-          yOffset: center.dy);
-      canvas.drawLine(offsetOuter, offsetInner, pHourMark);
-    }
+    _drawMarks(
+        paint: paintHourMark,
+        center: center,
+        canvas: canvas,
+        lineLingth: 20,
+        radius: r,
+        radSpacing: 2 * pi / 12);
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
     return true;
+  }
+
+  _drawMarks(
+      {required Paint paint,
+      required Offset center,
+      required Canvas canvas,
+      required double lineLingth,
+      required double radius,
+      required double radSpacing}) {
+    for (double rad = 0; rad < 2 * pi; rad = rad + radSpacing) {
+      final offsetOuter = _offsetOnCircle(
+          radius: radius, radians: rad, xOffset: center.dx, yOffset: center.dy);
+      final offsetInner = _offsetOnCircle(
+          radius: radius - lineLingth,
+          radians: rad,
+          xOffset: center.dx,
+          yOffset: center.dy);
+      canvas.drawLine(offsetOuter, offsetInner, paint);
+    }
   }
 
   _offsetOnCircle(
