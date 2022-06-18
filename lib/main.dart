9d2951ddd1b9cac53b141cd1ab1dbfb41c3843df
Dirk Zimmermann <me@dirkz.com>
2022-06-18 12:47:30 +0200

    Try fontFamily: "Serif"

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
       title: 'Russian Clock',
       theme: ThemeData(
         primarySwatch: Colors.grey,
+        fontFamily: "Serif",
       ),
       home: RussianClock(
         title: 'Russian Clock',
