2d607e3c1969741f30e23d5682709aa556691ab8
Dirk Zimmermann <me@dirkz.com>
2022-06-18 09:21:54 +0200

    Make it look better

--- lib/clock.dart
+++ lib/clock.dart
@@ -11,25 +11,29 @@ class Clock extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    final p = Paint();
-    p.style = PaintingStyle.stroke;
-    p.strokeWidth = 2;
+    final pCircle = Paint();
+    pCircle.style = PaintingStyle.stroke;
+    pCircle.strokeWidth = 4;
 
-    final r = min(size.height, size.width) / 2 - p.strokeWidth;
+    final r = min(size.height, size.width) / 2 - pCircle.strokeWidth;
     final center = Offset(size.width / 2, size.height / 2);
-    canvas.drawCircle(center, r, p);
+    canvas.drawCircle(center, r, pCircle);
 
-    const hourLineLength = 10;
+    final pHourMark = Paint();
+    pHourMark.style = PaintingStyle.fill;
+    pHourMark.strokeWidth = 4;
+
+    const hourLineLength = 20;
     const hourRadSpacing = 2 * pi / 12;
     for (double rad = 0; rad < 2 * pi; rad = rad + hourRadSpacing) {
-      final pOuter = pointOnCircle(
+      final offsetOuter = pointOnCircle(
           radius: r, radians: rad, xOffset: center.dx, yOffset: center.dy);
-      final pInner = pointOnCircle(
+      final offsetInner = pointOnCircle(
           radius: r - hourLineLength,
           radians: rad,
           xOffset: center.dx,
           yOffset: center.dy);
-      canvas.drawLine(pOuter, pInner, p);
+      canvas.drawLine(offsetOuter, offsetInner, pHourMark);
     }
   }
 
