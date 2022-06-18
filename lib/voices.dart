9e78cbef116004d35625c93b502b4d15e551dc32
Dirk Zimmermann <me@dirkz.com>
2022-06-18 14:26:01 +0200

    Start voices view

---
+++ lib/voices.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Voices extends StatefulWidget {
+  const Voices({super.key});
+
+  @override
+  State<Voices> createState() => _VoicesState();
+}
+
+class _VoicesState extends State<Voices> {
+  _VoicesState();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Title"),
+      ),
+      body: Text(""));
+  }
+}
