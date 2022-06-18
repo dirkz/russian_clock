14a468627ec2e9cb31b95dbf9a4437a7ca39d81e
Dirk Zimmermann <me@dirkz.com>
2022-06-18 12:13:14 +0200

    Skeleton for nominativeMinute

--- lib/russian_time.dart
+++ lib/russian_time.dart
@@ -177,4 +177,69 @@ class RussianTime {
         throw ArgumentError.value(n, "n");
     }
   }
+
+  static String nominativeMinute(int n) {
+    switch (n) {
+      case 1:
+        return "";
+      case 2:
+        return "";
+      case 3:
+        return "";
+      case 4:
+        return "";
+      case 5:
+        return "";
+      case 6:
+        return "";
+      case 7:
+        return "";
+      case 8:
+        return "";
+      case 9:
+        return "";
+      case 10:
+        return "";
+      case 11:
+        return "";
+      case 12:
+        return "";
+      case 13:
+        return "";
+      case 14:
+        return "";
+      case 15:
+        return "";
+      case 16:
+        return "";
+      case 17:
+        return "";
+      case 18:
+        return "";
+      case 19:
+        return "";
+      case 20:
+        return "";
+      case 21:
+        return "";
+      case 22:
+        return "";
+      case 23:
+        return "";
+      case 24:
+        return "";
+      case 25:
+        return "";
+      case 26:
+        return "";
+      case 27:
+        return "";
+      case 28:
+        return "";
+      case 29:
+        return "";
+      default:
+        throw ArgumentError.value(n, "n");
+    }
+  }
 }
